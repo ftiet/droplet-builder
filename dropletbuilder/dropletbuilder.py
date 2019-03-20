529de6e64a0ff6eaea391aed28cb06301dacc3ff
ftiet <ftiet21@gmail.com>
2019-03-20 17:17:42 -0500

    Initial droplet builder

--- dropletbuilder/dropletbuilder.py
+++ dropletbuilder/dropletbuilder.py
@@ -1,3 +1,9 @@
+import mbuild as mb
+import numpy as np
+from foyer import Forcefield 
+from ilforcefields.utils.utils import get_il
+from ilforcefields.utils.utils import get_ff
+
 """
 dropletbuilder.py
 Droplet on graphene builder
@@ -5,30 +11,77 @@ Droplet on graphene builder
 Handles the primary functions
 """
 
-
-def canvas(with_attribution=True):
+def get_height(r, theta):
     """
-    Placeholder function to show example docstring (NumPy format)
+    Helper function to get the height of a spherical cap
+    """
+    return r - r*np.cos(theta*np.pi/180)
 
-    Replace this function and doc string for your own project
+class GrapheneDroplet(mb.Compound):
+    """
+    Builds a droplet on a ~30x30 nm graphene sheet.
 
     Parameters
     ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
+    radius : int, default = 5
+        radius of the droplet in nm
+    angle : float, default = 90.0
+        contact angle of the droplet in degrees
 
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution
+    Attributes
+    ----------
+    see mbuild.Compound
+
     """
 
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
+    def __init__(self, radius=5, angle=90.0):
+        super(GrapheneDroplet, self).__init__()
 
+        cat = get_il('emim')
+        an = get_il('tf2n')
+        cmpnds = [cat, an, cat, an]
+        n_cmpnds = [150, 150, 150, 150]
 
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+        graphene = mb.load('./utils/graphene_sheet.pdb')
+        coords = list(graphene.periodicity)
+
+        height = get_height(radius, angle)
+        sheet_height = 0.682
+        sphere = mb.fill_sphere(compound=cmpnds, sphere=[coords[0], coords[1], radius, radius], n_compounds=n_cmpnds)
+
+        to_remove = []
+        for child in sphere.children:
+            for atom_coords in child.xyz:
+                if height > radius:
+                    if atom_coords[2] < height - radius:
+                        to_remove += child
+                        break
+                else:
+                    if atom_coords[2] < height:
+                        to_remove += child
+                        break
+
+        sphere.remove(to_remove)
+
+        sphere.xyz -= [0, 0, np.min(sphere.xyz, axis=0)[2]]
+        sphere.xyz += [0, 0, sheet_height + 0.2]
+
+        sheet1 = mb.clone(graphene)
+        sheet2 = mb.clone(graphene)
+        sheet3 = mb.clone(graphene)
+        sheet4 = mb.clone(graphene)
+        sheet2.translate([coords[0],0,0])
+        sheet3.translate([0,coords[1],0])
+        sheet4.translate([coords[0],coords[1],0])
+
+        GRAPHENE = Forcefield('./utils/graphene.xml')
+        LOPES = get_ff('lopes')
+
+        system = GRAPHENE.apply(sheet1) + GRAPHENE.apply(sheet2) + GRAPHENE.apply(sheet3) + GRAPHENE.apply(sheet4)
+        system = system + LOPES.apply(sphere.to_parmed(residues=['emim', 'tf2n']))
+
+        system.box[0] = coords[0]*10*2
+        system.box[1] = coords[1]*10*2
+        system.box[2] = 200
+        system.save('droplet.gro', overwrite=True)
+        system.save('droplet.top', overwrite=True, combine='all')
