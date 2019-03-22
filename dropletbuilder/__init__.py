97589a6274bcbaafffbf8c6e882995b5ca71053f
ftiet <ftiet21@gmail.com>
2019-03-22 16:18:46 -0500

    Package setup and util files

--- dropletbuilder/__init__.py
+++ dropletbuilder/__init__.py
@@ -4,11 +4,11 @@ Droplet on graphene builder
 """
 
 # Add imports here
-from .dropletbuilder import *
+from dropletbuilder.dropletbuilder import GrapheneDroplet
 
 # Handle versioneer
-from ._version import get_versions
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
+# from ._version import get_versions
+# versions = get_versions()
+# __version__ = versions['version']
+# __git_revision__ = versions['full-revisionid']
+# del get_versions, versions
