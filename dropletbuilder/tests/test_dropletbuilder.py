2a55e94f230baca1c3c5a8a1e09bd6757344a2e2
ftiet <ftiet21@gmail.com>
2019-03-22 17:28:38 -0500

    Initial tests

--- dropletbuilder/tests/test_dropletbuilder.py
+++ dropletbuilder/tests/test_dropletbuilder.py
@@ -3,10 +3,29 @@ Unit and regression test for the dropletbuilder package.
 """
 
 # Import package, test suite, and other packages as needed
-import dropletbuilder
 import pytest
 import sys
+import numpy as np
+
+@pytest.fixture
+def GrapheneDroplet():
+    from dropletbuilder.dropletbuilder import GrapheneDroplet
+    return GrapheneDroplet()
+
+"""
+Unit Tests for GrapheneDroplet class.
+"""
 
 def test_dropletbuilder_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "dropletbuilder" in sys.modules
+
+def test_save(GrapheneDroplet):
+    GrapheneDroplet.save('droplet.gro', overwrite=True)
+
+def test_particles_in_sheets(GrapheneDroplet):
+    for child in GrapheneDroplet.children:
+        if child.name != 'GPH':
+            if np.min(child.xyz, axis=0)[2] < GrapheneDroplet.surface_height + 0.001:
+                assert False
+    assert True
