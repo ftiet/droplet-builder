f63ea7b8d5a6414a2df8a9f650a1f0560a91e0b0
ftiet <ftiet21@gmail.com>
2019-03-27 16:08:44 -0500

    Switch to using mbuild.Lattice instead of loading in a graphene file

--- dropletbuilder/dropletbuilder.py
+++ dropletbuilder/dropletbuilder.py
@@ -34,7 +34,7 @@ def get_height(r, theta):
 
 class GrapheneDroplet(mb.Compound):
     """
-    Builds a droplet on a ~30x30 nm graphene sheet.
+    Builds a droplet on a graphene sheet.
 
     Parameters
     ----------
@@ -42,6 +42,10 @@ class GrapheneDroplet(mb.Compound):
         radius of the droplet in nm
     angle : float, default = 90.0
         contact angle of the droplet in degrees
+    x : float
+        dimension of graphene sheet in x direction in nm
+    y : float
+        dimension of graphene sheet in y direction in nm
 
     Attributes
     ----------
@@ -49,16 +53,44 @@ class GrapheneDroplet(mb.Compound):
 
     """
 
-    def __init__(self, radius=5, angle=90.0):
+    def __init__(self, radius=2, angle=90.0, x=None, y=None):
         super(GrapheneDroplet, self).__init__()
 
-        water = mb.load(get_fn('tip3p.mol2'))
-        graphene = mb.load(get_fn('graphene_sheet.pdb'))
-        coords = list(graphene.periodicity)
+        if x and y:
+            if x < radius * 4 or y < radius * 4:
+                raise ValueError('Dimensions of sheet must be at least radius * 4')
+        else:
+            if not x:
+                x = radius * 4
+            if not y:
+                y = radius * 4
 
+        factor = np.cos(np.pi/6)
+        # Estimate the number of lattice repeat units
+        replicate = [int(x/0.2456), int(y/0.2456)*(1/factor)]
+
+        carbon = mb.Compound(name='C')
+        lattice_spacing = [0.2456, 0.2456, 0.335]
+        angles = [90.0, 90.0, 120.0]
+        carbon_locations = [[0, 0, 0], [2/3, 1/3, 0]]
+        basis = {carbon.name : carbon_locations}
+        graphene_lattice = mb.Lattice(lattice_spacing=lattice_spacing, angles=angles, lattice_points=basis)
+        carbon_dict = {carbon.name : carbon}
+        graphene = graphene_lattice.populate(compound_dict=carbon_dict, x=replicate[0], y=replicate[1], z=3)
+
+        for particle in graphene.particles():
+            if particle.xyz[0][0] < 0:
+                particle.xyz[0][0] += graphene.periodicity[0]
+        graphene.periodicity[1] *= factor
+
+        sheet = mb.clone(graphene)
+        self.surface_height = np.max(sheet.xyz, axis=0)[2]
+        coords = list(sheet.periodicity)
+
+        water = mb.load(get_fn('tip3p.mol2'))
         height = get_height(radius, angle)
-        self.surface_height = np.max(graphene.xyz, axis=0)[2]
-        sphere = mb.fill_sphere(compound=[water], sphere=[coords[0], coords[1], radius, radius], n_compounds=[2000])
+        sphere_coords = [coords[0]/2, coords[1]/2, radius, radius]
+        sphere = mb.fill_sphere(compound=water, sphere=sphere_coords, density=997)
 
         to_remove = []
         for child in sphere.children:
@@ -74,28 +106,13 @@ class GrapheneDroplet(mb.Compound):
 
         sphere.remove(to_remove)
 
+        sheet.name = 'GPH'
         sphere.name = 'H2O'
         sphere.xyz -= [0, 0, np.min(sphere.xyz, axis=0)[2]]
         sphere.xyz += [0, 0, self.surface_height + 0.3]
 
-        sheet1 = mb.clone(graphene)
-        sheet2 = mb.clone(graphene)
-        sheet3 = mb.clone(graphene)
-        sheet4 = mb.clone(graphene)
-        sheet2.translate([coords[0],0,0])
-        sheet3.translate([0,coords[1],0])
-        sheet4.translate([coords[0],coords[1],0])
-
-        sheet1.name = 'GPH'
-        sheet2.name = 'GPH'
-        sheet3.name = 'GPH'
-        sheet4.name = 'GPH'
-
+        self.add(sheet)
         self.add(sphere)
-        self.add(sheet1)
-        self.add(sheet2)
-        self.add(sheet3)
-        self.add(sheet4)
-        self.periodicity[0] = coords[0]*2
-        self.periodicity[1] = coords[1]*2
-        self.periodicity[2] = 20
+        self.periodicity[0] = sheet.periodicity[0]
+        self.periodicity[1] = sheet.periodicity[1]
+        self.periodicity[2] = radius * 5
