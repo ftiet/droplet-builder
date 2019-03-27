5a06bf32c710a4afcb01c5516745c58d622ae6e7
ftiet <ftiet21@gmail.com>
2019-03-27 17:05:10 -0500

    Clean up code

--- dropletbuilder/dropletbuilder.py
+++ dropletbuilder/dropletbuilder.py
@@ -1,15 +1,9 @@
 import os
 import mbuild as mb
 import numpy as np
-from foyer import Forcefield 
+from foyer import Forcefield
 from pkg_resources import resource_filename
 
-"""
-dropletbuilder.py
-Droplet on graphene builder
-
-Handles the primary functions
-"""
 
 def get_fn(name):
     """
@@ -26,11 +20,13 @@ def get_fn(name):
         raise IOError('{} does not exist.'.format(fn))
     return fn
 
+
 def get_height(r, theta):
     """
     Helper function to get the height of a spherical cap
     """
-    return r - r*np.cos(theta*np.pi/180)
+    return r - r * np.cos(theta * np.pi / 180)
+
 
 class GrapheneDroplet(mb.Compound):
     """
@@ -58,25 +54,30 @@ class GrapheneDroplet(mb.Compound):
 
         if x and y:
             if x < radius * 4 or y < radius * 4:
-                raise ValueError('Dimensions of sheet must be at least radius * 4')
+                raise ValueError(
+                    'Dimensions of sheet must be at least radius * 4')
         else:
             if not x:
                 x = radius * 4
             if not y:
                 y = radius * 4
 
-        factor = np.cos(np.pi/6)
+        factor = np.cos(np.pi / 6)
         # Estimate the number of lattice repeat units
-        replicate = [int(x/0.2456), int(y/0.2456)*(1/factor)]
+        replicate = [int(x / 0.2456), int(y / 0.2456) * (1 / factor)]
 
         carbon = mb.Compound(name='C')
         lattice_spacing = [0.2456, 0.2456, 0.335]
         angles = [90.0, 90.0, 120.0]
-        carbon_locations = [[0, 0, 0], [2/3, 1/3, 0]]
-        basis = {carbon.name : carbon_locations}
-        graphene_lattice = mb.Lattice(lattice_spacing=lattice_spacing, angles=angles, lattice_points=basis)
-        carbon_dict = {carbon.name : carbon}
-        graphene = graphene_lattice.populate(compound_dict=carbon_dict, x=replicate[0], y=replicate[1], z=3)
+        carbon_locations = [[0, 0, 0], [2 / 3, 1 / 3, 0]]
+        basis = {carbon.name: carbon_locations}
+        graphene_lattice = mb.Lattice(
+            lattice_spacing=lattice_spacing,
+            angles=angles,
+            lattice_points=basis)
+        carbon_dict = {carbon.name: carbon}
+        graphene = graphene_lattice.populate(
+            compound_dict=carbon_dict, x=replicate[0], y=replicate[1], z=3)
 
         for particle in graphene.particles():
             if particle.xyz[0][0] < 0:
@@ -89,8 +90,9 @@ class GrapheneDroplet(mb.Compound):
 
         water = mb.load(get_fn('tip3p.mol2'))
         height = get_height(radius, angle)
-        sphere_coords = [coords[0]/2, coords[1]/2, radius, radius]
-        sphere = mb.fill_sphere(compound=water, sphere=sphere_coords, density=997)
+        sphere_coords = [coords[0] / 2, coords[1] / 2, radius, radius]
+        sphere = mb.fill_sphere(
+            compound=water, sphere=sphere_coords, density=997)
 
         to_remove = []
         for child in sphere.children:
