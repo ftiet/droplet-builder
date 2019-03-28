53f018eee1619fd2421f68d237771e801866d062
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-03-27 21:48:53 -0500

    Rework unit tests
    * Store unit tests in a class
    * Add initdir fixture
    * Make droplet fixtures smaller
    * Add `combine='all'` argument to GRO savers

--- dropletbuilder/tests/test_dropletbuilder.py
+++ dropletbuilder/tests/test_dropletbuilder.py
@@ -3,97 +3,91 @@ import sys
 import numpy as np
 
 
-@pytest.fixture
-def GrapheneDroplet():
-    from dropletbuilder.dropletbuilder import GrapheneDroplet
-    return GrapheneDroplet(radius=2, angle=90.0)
+class BaseTest:
+    @pytest.fixture(autouse=True)
+    def initdir(self, tmpdir):
+        tmpdir.chdir()
 
+    @pytest.fixture
+    def GrapheneDroplet(self):
+        from dropletbuilder.dropletbuilder import GrapheneDroplet
+        return GrapheneDroplet(radius=1, angle=90.0)
 
-@pytest.fixture
-def GrapheneDropletWithDims():
-    from dropletbuilder.dropletbuilder import GrapheneDroplet
-    return GrapheneDroplet(radius=2, angle=90.0, x=9, y=10)
+    @pytest.fixture
+    def GrapheneDropletWithDims(self):
+        from dropletbuilder.dropletbuilder import GrapheneDroplet
+        return GrapheneDroplet(radius=1, angle=90.0, x=4, y=4)
 
 
 """
 Unit Tests for GrapheneDroplet class.
 """
 
+class TestDropletBuilder(BaseTest):
+    def test_dropletbuilder_imported(self):
+        """Sample test, will always pass so long as import statement worked"""
+        assert "dropletbuilder" in sys.modules
 
-def test_dropletbuilder_imported():
-    """Sample test, will always pass so long as import statement worked"""
-    assert "dropletbuilder" in sys.modules
+    def test_save(self, GrapheneDroplet):
+        GrapheneDroplet.save('droplet.gro', overwrite=True, combine='all')
 
+    def test_save_with_dims(self, GrapheneDropletWithDims):
+        GrapheneDropletWithDims.save('droplet-with-dims.gro', overwrite=True, combine='all')
 
-def test_save(GrapheneDroplet):
-    GrapheneDroplet.save('droplet.gro', overwrite=True)
+    def test_hierarchy(self, GrapheneDroplet):
+        assert len(GrapheneDroplet.children) == 2
 
+    def test_hierarchy_with_dims(self, GrapheneDropletWithDims):
+        assert len(GrapheneDropletWithDims.children) == 2
 
-def test_save_with_dims(GrapheneDropletWithDims):
-    GrapheneDropletWithDims.save('droplet-with-dims.gro', overwrite=True)
-
-
-def test_hierarchy(GrapheneDroplet):
-    assert len(GrapheneDroplet.children) == 2
-
-
-def test_hierarchy_with_dims(GrapheneDropletWithDims):
-    assert len(GrapheneDropletWithDims.children) == 2
-
-
-def test_lateral_dims(GrapheneDroplet):
-    for child in GrapheneDroplet.children:
-        if (np.min(child.xyz, axis=0)[0] < 0
-                or np.min(child.xyz, axis=0)[1] < 0):
-            assert False
-        if (np.max(child.xyz, axis=0)[0] > GrapheneDroplet.periodicity[0] or
-                np.max(child.xyz, axis=0)[1] > GrapheneDroplet.periodicity[1]):
-            assert False
-    assert True
-
-
-def test_lateral_dims_with_dims(GrapheneDropletWithDims):
-    for child in GrapheneDropletWithDims.children:
-        if (np.min(child.xyz, axis=0)[0] < 0
-                or np.min(child.xyz, axis=0)[1] < 0):
-            assert False
-        if ((np.max(child.xyz, axis=0)[0] >
-             GrapheneDropletWithDims.periodicity[0])
-                or (np.max(child.xyz, axis=0)[1] >
-                    GrapheneDropletWithDims.periodicity[1])):
-            assert False
-    assert True
-
-
-def test_n_fluid_particles(GrapheneDroplet):
-    n_fluid_particles = 0
-    for child in GrapheneDroplet.children:
-        if child.name != 'GPH':
-            n_fluid_particles += child.n_particles
-    assert n_fluid_particles > 110 and n_fluid_particles < 1300
-
-
-def test_n_fluid_particles_with_dims(GrapheneDropletWithDims):
-    n_fluid_particles = 0
-    for child in GrapheneDropletWithDims.children:
-        if child.name != 'GPH':
-            n_fluid_particles += child.n_particles
-    assert n_fluid_particles > 110 and n_fluid_particles < 1300
-
-
-def test_fluid_particles_in_sheets(GrapheneDroplet):
-    for child in GrapheneDroplet.children:
-        if child.name != 'GPH':
-            if (np.min(child.xyz, axis=0)[2] <
-                    GrapheneDroplet.surface_height + 0.001):
+    def test_lateral_dims(self, GrapheneDroplet):
+        for child in GrapheneDroplet.children:
+            if (np.min(child.xyz, axis=0)[0] < 0
+                    or np.min(child.xyz, axis=0)[1] < 0):
                 assert False
-    assert True
-
-
-def test_fluid_particles_in_sheets_with_dims(GrapheneDropletWithDims):
-    for child in GrapheneDropletWithDims.children:
-        if child.name != 'GPH':
-            if (np.min(child.xyz, axis=0)[2] <
-                    GrapheneDropletWithDims.surface_height + 0.001):
+            if (np.max(child.xyz, axis=0)[0] > GrapheneDroplet.periodicity[0] or
+                    np.max(child.xyz, axis=0)[1] > GrapheneDroplet.periodicity[1]):
                 assert False
-    assert True
+        assert True
+
+    def test_lateral_dims_with_dims(self, GrapheneDropletWithDims):
+        for child in GrapheneDropletWithDims.children:
+            if (np.min(child.xyz, axis=0)[0] < 0
+                    or np.min(child.xyz, axis=0)[1] < 0):
+                assert False
+            if ((np.max(child.xyz, axis=0)[0] >
+                 GrapheneDropletWithDims.periodicity[0])
+                    or (np.max(child.xyz, axis=0)[1] >
+                        GrapheneDropletWithDims.periodicity[1])):
+                assert False
+        assert True
+
+    def test_n_fluid_particles(self, GrapheneDroplet):
+        n_fluid_particles = 0
+        for child in GrapheneDroplet.children:
+            if child.name != 'GPH':
+                n_fluid_particles += child.n_particles
+        assert n_fluid_particles > 20 and n_fluid_particles < 150
+
+    def test_n_fluid_particles_with_dims(self, GrapheneDropletWithDims):
+        n_fluid_particles = 0
+        for child in GrapheneDropletWithDims.children:
+            if child.name != 'GPH':
+                n_fluid_particles += child.n_particles
+        assert n_fluid_particles > 20 and n_fluid_particles < 150
+
+    def test_fluid_particles_in_sheets(self, GrapheneDroplet):
+        for child in GrapheneDroplet.children:
+            if child.name != 'GPH':
+                if (np.min(child.xyz, axis=0)[2] <
+                        GrapheneDroplet.surface_height + 0.001):
+                    assert False
+        assert True
+
+    def test_fluid_particles_in_sheets_with_dims(self, GrapheneDropletWithDims):
+        for child in GrapheneDropletWithDims.children:
+            if child.name != 'GPH':
+                if (np.min(child.xyz, axis=0)[2] <
+                        GrapheneDropletWithDims.surface_height + 0.001):
+                    assert False
+        assert True
